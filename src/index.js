9682a957bb1c94f6a3dc26d2649e0aea4741bf8b
Fanny J <fanny.jokinen@metropolia.fi>
2020-04-22 18:48:12 +0300

    väriteema

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Color theme:
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import {deepOrange} from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+       main: deepOrange[900],
+       light: deepOrange[500],
+      },
+      secondary: {
+        main: '#ffd822',
+        light: '#ffeb22',
+        dark: '#ffa122',
+      },
+      /* default: {
+        main: red[500],
+      }, */
+    },
+  });
+
+  ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App/>
+    </MuiThemeProvider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
