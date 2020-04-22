a97a145a567613d93c78224a4a621e72ef08784f
Nakki <nakkikone@Nakkikone.local>
2020-04-22 17:14:42 +0300

    project base init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Single from './views/Single';
+import Login from './views/Login';
+import Logout from './views/Logout';
+import {MediaProvider} from './contexts/MediaContext';
+import {Container} from '@material-ui/core';
+import Upload from './views/Upload';
+import MyFiles from './views/MyFiles';
+import Modify from './views/Modify';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // eslint-disable-next-line no-undef
+    <Router basename={process.env.PUBLIC_URL}>
+      <MediaProvider>
+        <Container maxWidth="md">
+          <Nav />
+          <main>
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/home" component={Home} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/single/:id" component={Single} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/upload" component={Upload} />
+              <Route path="/myfiles" component={MyFiles} />
+              <Route path="/modify/:id" component={Modify} />
+            </Switch>
+          </main>
+        </Container>
+      </MediaProvider>
+    </Router>
   );
-}
+};
 
 export default App;
