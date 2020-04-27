11af20700435e229a78ce91d4e02f02a39780436
Fanny J <fanny.jokinen@metropolia.fi>
2020-04-27 21:57:58 +0300

    front page comment sec

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -67,7 +67,7 @@ const useStyles = makeStyles((theme) => ({
   },
   expand: {
     transform: 'rotate(0deg)',
-    marginLeft: 'auto',
+    /* marginLeft: 'auto', */
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
