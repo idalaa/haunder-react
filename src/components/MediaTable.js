11ed644d6aabf2c5b9d32b1c22213dff8f58fb92
Fanny J <fanny.jokinen@metropolia.fi>
2020-04-27 21:56:10 +0300

    front page comment sec

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -15,6 +15,8 @@ import {
   CardActions,
   List,
   ListItem,
+  CardContent,
+  Typography,
 } from '@material-ui/core';
 import {MoreHoriz} from '@material-ui/icons';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
@@ -22,6 +24,9 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import { red } from '@material-ui/core/colors';
 import {MediaContext} from '../contexts/MediaContext';
 import {getAvatarImage} from '../hooks/ApiHooks';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+import Collapse from '@material-ui/core/Collapse';
 
 const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
@@ -51,13 +56,24 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: red[500],
   },
   container: {
-   /*  display: 'grid', */
+    display: 'grid',
     justifyContent: 'center',
     alignItems: 'center',
   },
   media: {
     width: '100%',
     height: '100%',
+    /* marginLeft: '33%' */
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(360deg)',
   },
 }));
 
@@ -66,6 +82,12 @@ const MediaTable = () => {
   const matches = useMediaQuery('(min-width:697px)');
 
   const picArray = useAllMedia();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 /*   const userPic = useAllMedia(kuva.avatar); */
 
 /* const [user] = useContext(MediaContext);
@@ -112,13 +134,35 @@ const MediaTable = () => {
                 </CardMedia>
                 
                 <CardActions disableSpacing>
-                  <IconButton aria-label="add to favorites">
+                  <IconButton aria-label="Add to favorites">
                     <FavoriteIcon />
                   </IconButton>
-                  <IconButton aria-label="comment">
+                  <IconButton 
+                    aria-label="Comment"
+                    className={clsx(classes.expand, {
+                      [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                  >
                     <ChatBubbleIcon />
                   </IconButton>
+                 {/*  <IconButton
+                    className={clsx(classes.expand, {
+                      [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton> */}
                 </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph>Comments:</Typography>
+                  </CardContent>
+                </Collapse>
               </Card>
               
             </ListItem>)
