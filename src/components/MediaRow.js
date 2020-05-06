895f65cf8ee6f370d652206b7be8cc52f30f41f1
Nakki <nakkikone@Nakkikone.local>
2020-05-06 18:48:18 +0300

    init

--- src/components/MediaRow.js
+++ src/components/MediaRow.js
@@ -2,14 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Link as RouterLink} from 'react-router-dom';
 import {
-  /* GridListTileBar,
   IconButton,
-  useMediaQuery,
   Card,
   CardHeader,
   Avatar,
-  CardMedia,*/
-  IconButton,
   makeStyles,
   CardActions,
   List,
@@ -25,9 +21,19 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 // import DeleteIcon from '@material-ui/icons/Delete';
 // import {deleteFile} from '../hooks/ApiHooks';
 import clsx from 'clsx';
+import {MoreHoriz} from '@material-ui/icons';
 import Collapse from '@material-ui/core/Collapse';
 import CommentTable from './CommentTable';
 import CommentForm from '../views/CommentForm';
+
+import Moment from 'react-moment';
+
+import moment from 'moment';
+import 'moment/locale/en-gb';
+import 'moment-timezone';
+
+moment.tz.add('Europe/Helsinki|HMT EET EEST|-1D.N -20 -30|0121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121|-1WuND.N OULD.N 1dA0 1xGq0 1cM0 1cM0 1cM0 1cN0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|12e5');
+
 
 const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
@@ -53,49 +59,98 @@ const MediaRow = ({file, myfiles}) => {
   if (file.thumbnails) {
     thumb = mediaUrl + file.thumbnails.w320;
   }
+
+  // Time functions
+  const laskeEro = (time) =>{
+    const date1 =new Date();
+    const date2 = new Date(time);
+
+    return Math.abs(date1 - date2) / /* 3.6e6 */ 3600000;
+  };
   return (
     <>
-      <ButtonBase component={RouterLink}
-        to={'/single/' + file.file_id}>
-        <img
-          src={thumb}
-          alt={file.title}
+      <ListItem key={file.file_id} className={classes.jaa}>
+        <Card className={classes.jaa}>
+          <CardHeader
+            avatar={
+                  file.avatar.length > 0 ? (
+                    <Avatar
+                      aria-label='user picture'
+                      className={classes.avatar}
+                      src={mediaUrl + file.avatar[0].filename}
+                      alt='Avatar image'
+                      title='Avatar image'
+                    />
+                  ) : (
+                    <Avatar
+                      aria-label='user picture'
+                      className={classes.avatar}
+                    />
+                  )
+            }
+            action={
+              <IconButton aria-label='settings'>
+                <MoreHoriz />
+              </IconButton>
+            }
+            title={
+                  file.user ? file.user.username : 'log in to see user data'
+            }
+            subheader={
+                  (laskeEro(file.time_added) >= 24) ? (
+                    console.log('TIME DD.MM.YYYY', file.time_added),
+                    <Moment tz='Europe/Helsinki' format='DD.MM.YYYY'>{file.time_added}</Moment>
+                  ) : (
+                    console.log('TIME FROM NOW', file.time_added),
+                    <Moment tz='Europe/Helsinki' fromNow>{file.time_added}</Moment>
+                  )
+                  // british backup time
+                  /* console.log("aika", file.time_added),
 
-          style={
-            {
-              filter: `
+                   moment(file.time_added).calendar() */
+            }
+          />
+          <ButtonBase component={RouterLink}
+            to={'/single/' + file.file_id}>
+            <img
+              src={thumb}
+              alt={file.title}
+
+              style={
+                {
+                  filter: `
                  brightness(${description.filters.brightness}%)
                  contrast(${description.filters.contrast}%) 
                  saturate(${description.filters.saturation}%)
                  sepia(${description.filters.sepia}%)
                  `,
-            }
-          }
-        />
-      </ButtonBase>
-      <List className={classes.list}>
-        <ListItem>
-          {file.title}
-        </ListItem>
-        <ListItem>
-          {myfiles ? '' : description.desc}
-        </ListItem>
-        <ListItem>
-          <CardActions disableSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton
-              aria-label="Comment"
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-            >
-              <ChatBubbleIcon />
-            </IconButton>
-            {/*  <IconButton
+                }
+              }
+            />
+          </ButtonBase>
+          <List className={classes.list}>
+            <ListItem>
+              {file.title}
+            </ListItem>
+            <ListItem>
+              {myfiles ? '' : description.desc}
+            </ListItem>
+            <ListItem>
+              <CardActions disableSpacing>
+                <IconButton aria-label="Add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton
+                  aria-label="Comment"
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                  })}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                >
+                  <ChatBubbleIcon />
+                </IconButton>
+                {/*  <IconButton
                     className={clsx(classes.expand, {
                       [classes.expandOpen]: expanded,
                     })}
@@ -105,17 +160,18 @@ const MediaRow = ({file, myfiles}) => {
                   >
                     <ExpandMoreIcon />
                   </IconButton> */}
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <CommentForm fileId = {file.file_id}/>
-              <Typography paragraph>Comments:</Typography>
-              <CommentTable file = {file.file_id}/>
-            </CardContent>
-          </Collapse>
-          {/* </Card> */}
-        </ListItem>
-      </List>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <CommentForm fileId = {file.file_id}/>
+                  <Typography paragraph>Comments:</Typography>
+                  <CommentTable file = {file.file_id}/>
+                </CardContent>
+              </Collapse>
+            </ListItem>
+          </List>
+        </Card>
+      </ListItem>
     </>
   );
 };
--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -2,52 +2,16 @@ import React from // useContext, useEffect, useState
   'react';
 import MediaRow from './MediaRow';
 import {useAllMedia} from '../hooks/ApiHooks';
-// import {MediaContext} from '../contexts/MediaContext';
-// import {getAvatarImage} from '../hooks/ApiHooks';
 
 import {
-  // GridList,
-  // GridListTile,
-  // ListSubheader,
-  // CardContent,
-  // Typography,
-  // CardActions,
   makeStyles,
   useMediaQuery,
-  Card,
-  CardHeader,
-  IconButton,
-  Avatar,
   CardMedia,
   List,
-  ListItem,
 } from '@material-ui/core';
-
-import {MoreHoriz} from '@material-ui/icons';
 import {red} from '@material-ui/core/colors';
 
-// import {MediaContext} from '../contexts/MediaContext';
-// import {getAvatarImage} from '../hooks/ApiHooks';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import clsx from 'clsx';
-// import Collapse from '@material-ui/core/Collapse';
-
-import Moment from 'react-moment';
-
-import moment from 'moment';
-import 'moment/locale/en-gb';
-import 'moment-timezone';
-
-moment.tz.add('Europe/Helsinki|HMT EET EEST|-1D.N -20 -30|0121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121|-1WuND.N OULD.N 1dA0 1xGq0 1cM0 1cM0 1cM0 1cN0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|12e5');
-
-/* import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import clsx from 'clsx';
-import Collapse from '@material-ui/core/Collapse';
-import Moment from 'react-moment'; */
-
-const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
+// const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -102,106 +66,20 @@ const MediaTable = () => {
   const matches = useMediaQuery('(min-width:697px)');
 
   const picArray = useAllMedia();
-  // const file = useAllMedia();
-  // const [expanded, setExpanded] = React.useState(false);
-
-
-  // Time functions
-  const laskeEro = (time) =>{
-    const date1 =new Date();
-    const date2 = new Date(time);
-
-    return Math.abs(date1 - date2) / /* 3.6e6 */ 3600000;
-  };
 
   return (
     <div className={classes.root}>
       <List
         cellheight={580}
         className={classes.gridList}
-        cols={matches ? 1 : 1}
-      >
-        {picArray.map((file) => (
-          <ListItem key={file.file_id} className={classes.jaa}>
-            <Card className={classes.jaa}>
-              <CardHeader
-                avatar={
-                  file.avatar.length > 0 ? (
-                    <Avatar
-                      aria-label='user picture'
-                      className={classes.avatar}
-                      src={mediaUrl + file.avatar[0].filename}
-                      alt='Avatar image'
-                      title='Avatar image'
-                    />
-                  ) : (
-                    <Avatar
-                      aria-label='user picture'
-                      className={classes.avatar}
-                    />
-                  )
-                }
-                action={
-                  <IconButton aria-label='settings'>
-                    <MoreHoriz />
-                  </IconButton>
-                }
-                title={
-                  file.user ? file.user.username : 'log in to see user data'
-                }
-                subheader={
-                  (laskeEro(file.time_added) >= 24) ? (
-                    console.log('TIME DD.MM.YYYY', file.time_added),
-                    <Moment tz='Europe/Helsinki' format='DD.MM.YYYY'>{file.time_added}</Moment>
-                  ) : (
-                    console.log('TIME FROM NOW', file.time_added),
-                    <Moment tz='Europe/Helsinki' fromNow>{file.time_added}</Moment>
-                  )
-                  // british backup time
-                  /* console.log("aika", file.time_added),
+        cols={matches ? 1 : 1}>
+        {
+          picArray.map((file) =>
 
-                   moment(file.time_added).calendar() */
-                }
-              />
-
-              <CardMedia className={classes.container}>
-                <MediaRow className={classes.media} file={file} />
-              </CardMedia>
-
-              {/* <CardActions disableSpacing>
-                  <IconButton aria-label="Add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="Comment"
-                    className={clsx(classes.expand, {
-                      [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                  >
-                    <ChatBubbleIcon />
-                  </IconButton>
-                   <IconButton
-                    className={clsx(classes.expand, {
-                      [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph>Comments:</Typography>
-                    <CommentTable />
-                  </CardContent>
-                </Collapse> */}
-            </Card>
-          </ListItem>
-        ))}
+            <CardMedia key={file.file_id} className={classes.container}>
+              <MediaRow className={classes.media} file={file} />
+            </CardMedia>)
+        }
       </List>
     </div>
   );
