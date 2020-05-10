fe0521bc41fa0577a953d37fb7d9616e01a037e7
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-05-10 21:54:45 +0300

    groups

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
-import {Button} from '@material-ui/core';
+import { makeStyles, Button } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  body: {
+    backgroundImage: 'url(./img/haundrlogo.png)',
+  },
+}));
 const Login = () => {
+  const classes = useStyles();
   const [toggle, setToggle] = useState(true);
   // funktio jolla setToggle true/false
   const showHide = () => {
@@ -11,10 +17,11 @@ const Login = () => {
   };
   return (
     <>
-      {toggle ?
-        <LoginForm/> :
-        <RegisterForm/>
-      }
+      {toggle ? (
+        <LoginForm className={classes.body} />
+      ) : (
+        <RegisterForm className={classes.body} />
+      )}
       <Button onClick={showHide}>{toggle ? 'or register' : 'or login'}</Button>
     </>
   );
