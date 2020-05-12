7bdd2e9a2773164a29584ad9a87f3dff60bfcbac
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-05-12 20:58:03 +0300

    Font change

--- src/App.js
+++ src/App.js
@@ -17,35 +17,46 @@ import Groups from './views/Groups';
 import Search from './views/Search';
 import MySingle from './views/MySingle';
 import Favourite from './views/Favourite';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+const font = "'Lato', sans-serif";
+
+const muiTheme = createMuiTheme({
+  fontFamily: font,
+});
 
 const App = () => {
   return (
-    // eslint-disable-next-line no-undef
-    <Router basename={process.env.PUBLIC_URL}>
-      <MediaProvider>
-        <CommentProvider>
-          <Container maxWidth='md'>
-            <Nav />
-            <main>
-              <Switch>
-                <Route path='/' exact component={Login} />
-                <Route path='/home' component={Home} />
-                <Route path='/profile' component={Profile} />
-                <Route path='/single/:id' component={Single} />
-                <Route path='/logout' component={Logout} />
-                <Route path='/upload' component={Upload} />
-                <Route path='/myfiles' component={MyFiles} />
-                <Route path='/modify/:id' component={Modify} />
-                <Route path='/groups' component={Groups} />
-                <Route path='/search' component={Search} />
-                <Route path='/mysingle/:id' component={MySingle} />
-                <Route path='/favourite' component={Favourite} />
-              </Switch>
-            </main>
-          </Container>
-        </CommentProvider>
-      </MediaProvider>
-    </Router>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <div style={{ fontFamily: font }}>
+        // eslint-disable-next-line no-undef
+        <Router basename={process.env.PUBLIC_URL}>
+          <MediaProvider>
+            <CommentProvider>
+              <Container maxWidth='md'>
+                <Nav />
+                <main>
+                  <Switch>
+                    <Route path='/' exact component={Login} />
+                    <Route path='/home' component={Home} />
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/single/:id' component={Single} />
+                    <Route path='/logout' component={Logout} />
+                    <Route path='/upload' component={Upload} />
+                    <Route path='/myfiles' component={MyFiles} />
+                    <Route path='/modify/:id' component={Modify} />
+                    <Route path='/groups' component={Groups} />
+                    <Route path='/search' component={Search} />
+                    <Route path='/mysingle/:id' component={MySingle} />
+                    <Route path='/favourite' component={Favourite} />
+                  </Switch>
+                </main>
+              </Container>
+            </CommentProvider>
+          </MediaProvider>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
