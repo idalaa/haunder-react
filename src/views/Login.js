f9f4ba0401d2cdd061d5e4057ba122d57be3ed5b
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-05-12 19:29:33 +0300

    Fixes

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
-import { makeStyles, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import '../index.css';
-import styled from 'styled-components';
 
 const Login = () => {
   const [toggle, setToggle] = useState(true);
