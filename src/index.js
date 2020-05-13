64fc10127ebac3597f329e45c98867145bcd9c38
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-05-13 11:44:06 +0300

    Font change

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Color theme:
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import {deepOrange} from '@material-ui/core/colors';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Noto Sans, Sans serif',
+  },
   palette: {
     primary: {
       main: deepOrange[900],
@@ -19,17 +24,16 @@ const theme = createMuiTheme({
       light: '#ffeb22',
       dark: '#ffa122',
     },
-    /* default: {
-        main: red[500],
-      }, */
   },
 });
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-      <App/>
-    </MuiThemeProvider>,
-    document.getElementById('root'));
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
